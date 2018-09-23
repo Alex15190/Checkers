f5bafc1936f8d371238ee0cc7b12f556f9754c6d
Alex Chekodanov <checkodanov@gmail.com>
2018-09-23 14:19:37 +0300

    Initial Commit

---
+++ Checkers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Checkers
+//
+//  Created by Alex on 23.09.2018.
+//  Copyright © 2018 Alex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
